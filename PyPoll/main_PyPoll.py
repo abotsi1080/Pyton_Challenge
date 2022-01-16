6b04be861dc39bc6a5c4c9f2684b69d048be4aa5
abotsi1080 <95152020+abotsi1080@users.noreply.github.com>
2022-01-16 17:40:15 -0500

    almost done with the homework

--- PyPoll/main_PyPoll.py
+++ PyPoll/main_PyPoll.py
@@ -1,57 +1,77 @@
+# Importing the operating system module
 import os
+# Importing the csv file needed to work with
 import csv
+#Calling or setting the csv file path
+election_data_csv  = os.path.join("Resources", "election_data.csv")
 
-election_data_csv = os.path.join("Resources", "election_data.csv")
-
-total_number_votes = 0 
+# Defining the variable list
+total_number_votes = 0
 list_candidate = []
-dict_candidate = {}
+unique_candidate = []
+Khan_Vote = 0
+Correy_Vote = 0
+Li_Vote = 0
+OTooley_Vote = 0
+vote_tally = [Khan_Vote, Correy_Vote, Li_Vote, OTooley_Vote]
+# Open the csv file to read  
+with open(election_data_csv ) as csv_file:
 
-winning_vote = 0 
-winner = ""
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    csv_header = next(csv_reader)
+    # Reading through each row of data after reading the header
+    for row in csv_reader:
+       # Counting the total votes casted  
+       total_number_votes += 1
 
-with open(election_data_csv) as csv_file:
+       #listing candidates with vote
+       list_candidate.append(row[2])
 
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        csv_header = next(csv_reader)
- 
-        for row in csv_reader:
-            # Counting the total votes casted    
-            total_number_votes += 1 
-        
-            # Getting the candidate name from the row 
-            candidate = row[2]
-
-            # begin the conditional if statement 
-            if candidate not in list_candidate:
-            # add the candidate to the list_candidate 
-                list_candidate.append(candidate)
-                dict_candidate[candidate] = 0
-            
-            dict_candidate[candidate] +=1 
 # Looping through the candidates to calculate the percentage votes
-for candidate in dict_candidate:
-    percentage_vote = round(float(dict_candidate[candidate])/float(total_number_votes), 2)
+for name in list_candidate:
+        if name not in unique_candidate:
+            unique_candidate.append(name)
 
-    votes = dict_candidate[candidate]
-    if votes > winning_vote:
-            winning_vote = votes
-            winner = candidate
+        #starting new conditional statement to get the votes count for candidates
+        if name == 'Khan':
+            Khan_Vote += 1
 
-    output = "Analysis/election_results.txt"
-    with open(output, "w") as txtfile:
+        elif name == 'Correy':
+            Correy_Vote += 1
+        
+        elif name == 'Li':
+            Li_Vote += 1
 
-# Writing the voting resutls to text file
-        txtfile.write(f"Election Results" + "\n")
-        txtfile.write(f"--------------------------------------" + "\n")
-        txtfile.write(f"Total Votes: {total_number_votes}" + "\n")
-        txtfile.write(f"--------------------------------------" + "\n")
-        txtfile.write(f"{candidate}: {percentage_vote} {dict_candidate[candidate]}\n")
-    #datafile.write(f"{candidate}: {percentage:.3%} ({dict_candidate[candidate]})\n")
+        else:
+            OTooley_Vote += 1
+
+winning_vote_count = max(vote_tally)
+winner = unique_candidate[vote_tally.index(winning_vote_count)]
+
+ # Writing the voting resutls to text file
+output = "Analysis/election_results.txt"
+with open(output, "w") as txtfile:
+    txtfile.write(f"Election Results" + "\n")
+    txtfile.write(f"-------------------------" + "\n")  
+    txtfile.write(f"Total Votes: {total_number_votes}" + "\n")
+    txtfile.write(f"-------------------------" + "\n")
+    txtfile.write(f"Khan: {round((Khan_Vote/total_number_votes)*100, )}% ({Khan_Vote})" + "\n")      
+    txtfile.write(f"Correy: {round((Correy_Vote/total_number_votes)*100, 2)}% ({Correy_Vote})" + "\n")     
+    txtfile.write(f"Li: {round((Li_Vote/total_number_votes)*100, 2)}% ({Li_Vote})" + "\n")     
+    txtfile.write(f"O'Tooley: {round((OTooley_Vote/total_number_votes)*100, 2)}% ({OTooley_Vote})" + "\n")
+    txtfile.write(f"-------------------------" + "\n") 
+    txtfile.write(f"Winner: {winner}" + "\n")  
+    txtfile.write(f"-------------------------" + "\n")
 
 # Printing the voting results for viewing
-    print(f"Election Results")
-    print(f"--------------------------------------")
+    print("Election Results")
+    print("-------------------------")  
     print(f"Total Votes: {total_number_votes}")
-    print(f"--------------------------------------")
-    print(f"{candidate}: {percentage_vote} {dict_candidate[candidate]}")
+    print("-------------------------")
+    print(f"Khan: {round((Khan_Vote/total_number_votes)*100, 2)}% ({Khan_Vote})")      
+    print(f"Correy: {round((Correy_Vote/total_number_votes)*100, 2)}% ({Correy_Vote})")     
+    print(f"Li: {round((Li_Vote/total_number_votes)*100, 2)}% ({Li_Vote})")     
+    print(f"O'Tooley: {round((OTooley_Vote/total_number_votes)*100, 2)}% ({OTooley_Vote})")
+    print("-------------------------") 
+    print(f"Winner: {winner}")  
+    print("-------------------------")
